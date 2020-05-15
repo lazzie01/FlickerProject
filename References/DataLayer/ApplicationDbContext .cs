13354c3851d2a8ff3145d7a7ae07a2e2ecc6bf98
lazzie <munetsilazzie@gmail.com>
2020-05-15 17:39:10 +0200

    cleaning

---
+++ References/DataLayer/ApplicationDbContext .cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.; database=FlickerProjectDB; Integrated Security=True;");
+        }
+
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Landmark> Landmarks { get; set; }
+        public DbSet<UserLocation> UserLocations { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
--- References/DataLayer/Models.cs
+++ References/DataLayer/Models.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace DataLayer
 {
@@ -60,29 +59,6 @@ namespace DataLayer
         public string Password { get; set; }
 
         public virtual ICollection<UserLocation> UserLocations { get; set; }
-
-    }
-
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
-
-        public ApplicationDbContext() : base()
-        {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //optionsBuilder.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
-            optionsBuilder.UseSqlServer(@"Data Source=.; database=FlickerProjectDB; Integrated Security=True;");
-        }
-
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<Landmark> Landmarks { get; set; }
-        public DbSet<UserLocation> UserLocations { get; set; }
-        public DbSet<User> Users { get; set; }
 
     }
 }
